488ab3ad525ddc81bccd1443c00441b1af2c5e17
Jeff Austin <jeffaustin32@gmail.com>
2018-03-03 12:10:22 -0800

    Updated Flood FIll to have a limit on depth

--- middleware/floodFill.js
+++ middleware/floodFill.js
@@ -1,5 +1,6 @@
 var boardUtils = require('../utilities/board');
 var Point = require('../classes/point');
+var config = require('../config.json');
 
 module.exports = function (req, res, next) {
     // Get the vertex for the head of our snake
@@ -19,7 +20,7 @@ function floodFill(req, vertex, depth) {
     vertex.visited = true;
 
     // If no children
-    if (vertex.outEdges.length === 0) {
+    if (vertex.outEdges.length === 0 || depth === config.boardValues.floodFillLimit) {
         // Reset visisted property so other paths can reach this vertex
         vertex.visited = false;
         // Return depth
